d71c719ee23fe6b553deb944b52a431fb7707034
Titweety <darksword680@gmail.com>
2024-11-25 09:13:12 +0800

    adding sql file

--- ghafschool.sql
+++ ghafschool.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2024 at 01:41 AM
+-- Generation Time: Nov 25, 2024 at 02:12 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -28,9 +28,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `students` (
-  `name` varchar(250) NOT NULL,
-  `course_id` varchar(11) NOT NULL,
-  `id` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `student_id` varchar(250) NOT NULL,
+  `name` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
